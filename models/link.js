10e1930ac37c8536cea7dbaaffa23ed932713f17
Daniel Ik <codingeinstein@gmail.com>
2022-04-19 19:23:37 +0100

    updated link.js

--- models/link.js
+++ models/link.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const LinkModel = mongoose.model("Link",
+const LinkModel = mongoose.model(
+  "Link",
   new mongoose.Schema({
     orignialUrl: String,
-    generatedUrl: { type: String, default: Date.now },
+    generatedUrl: { type: String, default: generateRandomUrl },
+    time: { type: Date, default: Date.now },
+    clicks: {type: Number, default:0},
   })
 );
 
-
-module.exports = {LinkModel};
+module.exports = { LinkModel };
