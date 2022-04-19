5106add2e75c7635c3b8dcdfb73541366e310d07
Daniel Ik <codingeinstein@gmail.com>
2022-04-19 12:57:24 +0100

    connect to mongodb server

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/url_shortner')
+.then(()=>{
+    console.log('connected to database successfully.');
+})
+.catch((err)=>{
+    console.log(`An error occurred -> ${err}`);
+});
 
 const app = express();
 
