a3a6e11afcae610050b121488afa3fd13ef5dc4a
Daniel Ik <codingeinstein@gmail.com>
2022-04-19 20:03:00 +0100

    generateRandomUrl function

--- models/link.js
+++ models/link.js
@@ -1,5 +1,19 @@
 const mongoose = require("mongoose");
 
+const generateRandomUrl = ()=>{
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz1234567890';
+    let out = '';
+
+    for (let i=0;i<9;i++){
+        const rn = Math.floor(Math.random() * chars.length);
+        out = out+chars[rn];
+
+    };
+
+    return out;
+
+};
+
 const LinkModel = mongoose.model(
   "Link",
   new mongoose.Schema({
